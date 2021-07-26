d69fc4c891db2c4f28660dd87ac497448792fab1
newsalaar <keyshet@gmail.com>
2021-07-26 15:45:01 +0300

    changed validation parameters

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -45,8 +45,8 @@ const Form = ({ setBikes }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' required='required' pattern='([a-z]|[A-Z]){5,30}' title='5 to 30 letters' />
-      <input value={type} onChange={(e) => setType(e.target.value)} placeholder='Type' required='required' pattern='([a-z]|[A-Z]){5,30}' title='5 to 30 letters' />
+      <input value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' required='required' pattern='([a-z]|[A-Z]|[0-9]){5,30}' title='5 to 30 letters' />
+      <input value={type} onChange={(e) => setType(e.target.value)} placeholder='Type' required='required' pattern='([a-z]|[A-Z]|[0-9]){5,30}' title='5 to 30 letters' />
       <br />
       <input value={color} onChange={(e) => setColor(e.target.value)} placeholder='Color' required='required' pattern='([a-z]|[A-Z]){3,15}' title='3 to 15 letters' />
       <input value={size} onChange={(e) => setSize(e.target.value)} placeholder='Wheel size' required='required' pattern='[0-9]+' title='only a number' />
