d53d0de3ab04d5594f86d017805e27a287005793
xaarxus <33036302+xaarxus@users.noreply.github.com>
2021-07-30 14:30:11 +0300

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -48,9 +48,9 @@ const Form = ({ setBikes }) => {
       <input value={type} onChange={(e) => setType(e.target.value)} placeholder='Type' required='required' pattern='([a-z]|[A-Z]|[0-9]){5,30}' title='5 to 30 letters' />
       <br />
       <input value={color} onChange={(e) => setColor(e.target.value)} placeholder='Color' required='required' pattern='([a-z]|[A-Z]){3,15}' title='3 to 15 letters' />
-      <input value={size} onChange={(e) => setSize(e.target.value)} placeholder='Wheel size' required='required' pattern='[0-9]+' title='only a number' />
+      <input value={size} onChange={(e) => setSize(e.target.value)} placeholder='Wheel size' required='required' pattern='[0-9]+\.[0-9]+|[0-9]+' title='only a number' />
       <br />
-      <input value={price} onChange={(e) => setPrice(e.target.value)} placeholder='Price' required='required' pattern='[0-9]+' title='only a number' />
+      <input value={price} onChange={(e) => setPrice(e.target.value)} placeholder='Price' required='required' pattern='[0-9]+\.[0-9]+|[0-9]+' title='only a number' />
       <input value={id} onChange={(e) => setId(e.target.value)} placeholder='ID (slug): XXXXXXXXXXXXX' required='required' pattern='([a-z]|[A-Z]|[0-9]){13,20}' title='13 to 20 characters' />
       <br />
       <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder='Description' />
